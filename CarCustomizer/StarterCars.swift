a9aced9f7f342ed9735e756181dcb71c758a3a36
Anish Kochhar <anishk12@icloud.com>
2020-01-12 22:40:02 +0000

    Pushed Starter Cars

---
+++ CarCustomizer/StarterCars.swift
@@ -0,0 +1,18 @@
+//
+//  StarterCars.swift
+//  CarCustomizer
+//
+//  Created by Anish Kochhar on 12/01/2020.
+//  Copyright © 2020 Anish Kochhar. All rights reserved.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make: "Tesla", model: "Model X", topSpeed: 190, acceleration: 5.3, handling: 8),
+                Car(make: "BMW", model: "i8", topSpeed: 220, acceleration: 5.7, handling: 7),
+                Car(make: "Alpha Romeo", model: "Spider", topSpeed: 172, acceleration: 6.2, handling: 7),
+                Car(make: "Range Rover", model: "Sport", topSpeed: 145, acceleration: 7.8, handling: 8),
+                Car(make: "Aston Martin", model: "DB11", topSpeed: 230, acceleration: 5.6, handling: 6),
+                Car(make: "Porsche", model: "911", topSpeed: 200, acceleration: 4.9, handling: 7)]
+}
